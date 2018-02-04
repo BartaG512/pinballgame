e14987b962e725ea56cb011b3a04b04a5e57ff10
BartaG512 <ecco512@gmail.com>
2018-02-04 19:08:05 +0100

    second commit

--- gameUI.js
+++ gameUI.js
@@ -1,4 +1,4 @@
-// function GameUI() {
+
 //   const canvas = document.querySelector('canvas');
 //   const mapWidth = 300;
 //   const mapHeight = 500;
@@ -11,72 +11,14 @@
 //     mapHeight: mapHeight,
 //     fps: fps
 //   });
-// }
-//
-// const game12 = new GameUI();
+
 //
 //
 // // module aliases
 //
 //
 // let canvas = document.querySelector('canvas');
-
-let worldWidth = 400;
-let worldHeight = 500;
-let offset = 1;
-//
- var Engine = Matter.Engine,
-     Render = Matter.Render,
-     World = Matter.World,
-     Bodies = Matter.Bodies;
-
- // create an engine
- var engine = Engine.create();
-//
- // create a renderer
- var render = Render.create({
-     element: document.body,
-     engine: engine,
-     options: {
-      width: worldWidth,
-      height: worldHeight,
-      background: '#fff',
-      wireframes: true,
-    }
- });
-
-engine.world.gravity.x = 1;
-engine.world.gravity.y = 0;
-
-World.add(engine.world, [
-   Bodies.rectangle(worldWidth / 2, worldHeight - offset, worldWidth, 1, {
-     isStatic: true,
-     render: {
-       fillStyle: "#FFFFFF"
-     }
-   }),
-   Bodies.rectangle(worldWidth / 2, offset, worldWidth, 1, {
-     isStatic: true,
-     render: {
-       fillStyle: "#FFFFFF"
-     }
-   }),
-   Bodies.rectangle(offset, worldHeight / 2, 1, worldHeight, {
-     isStatic: true,
-     render: {
-       fillStyle: "#FFFFFF"
-     }
-   }),
-   Bodies.rectangle(worldWidth - offset, worldHeight / 2, 1, worldHeight, {
-     isStatic: true,
-     render: {
-       fillStyle: "#FFFFFF"
-     }
-   })
- ]);
-
- // run the engine
- Engine.run(engine);
-
- // run the renderer
- Render.run(render);
+var game = new PinballGame( {
+  mapHeight: 800,
+  mapWidth: 200
+} );
--- pinball.js
+++ pinball.js
@@ -1,143 +1,155 @@
-function PinballGame({ canvas, mapWidth, mapHeight, fps }) {
-  // Canvas
+function PinballGame({ mapWidth, mapHeight }) {
+  this.worldWidth = mapWidth;
+  let worldWidth = this.worldWidth;
+  this.worldHeight = mapHeight;
+  let worldHeight = this.worldHeight;
+  let offset = -10;
+  const Engine = Matter.Engine;
+  const Render = Matter.Render;
+  const World = Matter.World;
+  const Mouse = Matter.Mouse;
+  const MouseConstraint = Matter.MouseConstraint;
+  this.Bodies = Matter.Bodies;
+  // create an engine
+  this.engine = Engine.create();
+  engine = this.engine;
+  // create a renderer
+  var render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+      width: worldWidth,
+      height: worldHeight,
+      background: '#fffff',
+      wireframes: false,
+    }
+  });
+
+  engine.world.gravity.x = 0.0;
+  engine.world.gravity.y = 0.0;
+
+
+    this.ship = {
+      size: {w: 100, h: 20},
+      pos: {x: mapWidth/2, y: mapHeight-20},
+      color: "#ff0000"
+    };
+
+  World.add(engine.world, [
+
+    //SHIP
+     this.Bodies.rectangle(worldWidth / 2, worldHeight - 30, this.ship.size.w, this.ship.size.h, {
+       isStatic: true,
+       label: "Bottom-Border",
+       render: {
+         fillStyle: this.ship.color,
+       }
+     }),
+     // CREATE BORDERS
+     this.Bodies.rectangle(worldWidth / 2, offset, worldWidth, 10, {
+       isStatic: true,
+       label: "TOP-Border",
+       render: {
+         fillStyle: "#FFFFFF"
+       }
+     }),
+     this.Bodies.rectangle(offset, worldHeight / 2, -offset, worldHeight, {
+       isStatic: true,
+       label: "LEFT-Border",
+       render: {
+         fillStyle: "#FFFFFF"
+       }
+     }),
+     this.Bodies.rectangle(worldWidth - offset, worldHeight / 2, offset, worldHeight, {
+       isStatic: true,
+       label: "RIGHT-Border",
+       render: {
+         fillStyle: "#FFFFFF"
+       }
+     })
+
+  ]);
+  //CREATE BALLS
+  this.balls = [];
+  for (var i = 1; i <= 2; i++) {
+    World.add(engine.world, this.addNewBall(worldWidth/2, worldHeight/2, i+1, i+1, "white", 5, 1));
+  }
+  console.log("engine.world",engine.world);
+
+  const canvas = document.querySelector('canvas');
   this.canvas = canvas;
   this.cxt = this.canvas.getContext ("2d");
-  this.canvas.width = mapWidth;
-  this.canvas.height = mapHeight;
-  // FPS//difficulty
-  this.fps = fps;// frame per second
-  // speedChangeRequest
-  this.speedChangeRequest = false;
-
+  //
   this.mouse = {
     x: 0,
     y: 0
   };
-  //Create first ball
-  this.balls = [];
-    for (var i = 1; i <= 7; i++) {
-      this.addNewBall(mapWidth/10*i  , mapHeight/10*i, i+1, i+1, "white", 20, 1);
-    }
-  this.startContinueGame();
 
-  this.ship = {
-    size: {w: 100, h: 20},
-    pos: {x: mapWidth/2, y: mapHeight-20},
-    color: "red"
-  };
+
+  let mouse = Mouse.create(render.canvas);
+  let mouseConstraint = MouseConstraint.create(engine, {
+    mouse: mouse,
+    constraint: {
+      //  bodyB: this.balls[0],
+        stiffness: 1,
+        damping: 0,
+        render: {
+            visible: false
+        }
+    }
+  });
+
+  World.add(engine.world, mouseConstraint);
+
+  //World.add(engine.world, mouseConstraint);
+  // keep the mouse in sync with rendering
+  //render.mouse = mouse;
 
   this.mouseCTRL = new MouseCTRL({
     canvas: canvas,
     updateMouseCoordinates: (mousePos) => { this.updateMouseCoordinates(mousePos); }
   });
 
+  // run the engine
+  Engine.run(engine);
+  // run the renderer
+  Render.run(render);
 };
 
 PinballGame.prototype.addNewBall = function(x, y, vx, vy, color, size, damage ) {
-  let ball = new Ball({
-    canvas: this.canvas,
-    pos: {x: x, y: y},
-    velocity: {x: vx, y: vy},
-    color: color,
-    size: size,
-    damage: damage
+  let ball = this.Bodies.circle(x, y, size, {
+    density: 1.01,
+    frictionAir : 0.0,
+    friction: 0,
+    inertia : Infinity,
+    restitution: 1,
+    render: {
+      fillStyle: color,
+    }
   });
+  Matter.Body.setVelocity(ball, {x: 0, y: 10});
+  ball.damage = damage;
   this.balls.push(ball);
+  console.log("this.balls", this.balls);
+  return ball;
 };
-
+//
 PinballGame.prototype.updateMouseCoordinates = function(mousePos) {
   this.mouse = {
     x: mousePos.x,
     y: mousePos.y
   };
-  console.log(this.mouse);
-
-};
-
-PinballGame.prototype.gameLoop = function() {
-  this.clearBoard();
-  this.updateGame();
-  this.drawGame();
-};
-
-PinballGame.prototype.updateGame = function() {
-  for (var i = 0; i < this.balls.length; i++) {
-    this.balls[i].move();
-  }
-  // move the ball
-
-  /// CHECK collosion
-  // LEFT SIDE of the wall
-  if (this.mouse.x - this.ship.size.w/2 < 0){
-    this.ship.pos.x = this.ship.size.w;
+  let distanceX = game.balls[0].position.x - this.mouse.x;
+  let force = distanceX * 300;
+  let distanceY = game.balls[0].position.y - this.mouse.y;
+  let shipBody = this.engine.world.bodies[0];
+  console.log("this.mouse.x < (this.ship.w/2), ", this.mouse.x < (this.ship.size.w/2), "ship", (this.ship.size.w / 2), "mouse.x", this.mouse.x);
+  if (this.mouse.x < (this.ship.size.w/2)) {
+    Matter.Body.setPosition(shipBody, {x : this.ship.size.w/2, y: this.engine.world.bodies[0].position.y});
+  } else if (this.mouse.x > this.worldWidth - this.ship.size.w/2) {
+    Matter.Body.setPosition(shipBody, {x : this.worldWidth - this.ship.size.w / 2, y: this.engine.world.bodies[0].position.y});
   } else {
-    this.ship.pos.x = this.mouse.x + this.ship.size.w/2;
+    Matter.Body.setPosition(shipBody, {x : this.mouse.x, y : this.engine.world.bodies[0].position.y});
   }
- // COLLOSION WITH RIGHT SIDE OF THE WALL
-  if (this.mouse.x + this.ship.size.w/2 > this.canvas.width){
-    this.ship.pos.x = this.canvas.width;
-  }
-  // CHECK BALL POSITION
-
+//  //Matter.Body.setPosition(game.balls[0], {x : this.mouse.y, y : this.mouse.y}, {x: force, y: this.worldHeight - 30});
 };
-
-
-PinballGame.prototype.pauseGame = function() {
-  if (this.SessionId) {
-    clearInterval(this.SessionId);
-  } // pause game
-  this.isRunning = false;
-};
-
-PinballGame.prototype.clearBoard = function() {
-    this.cxt.clearRect (0, 0, this.canvas.width, this.canvas.height);
-};
-
-PinballGame.prototype.pauseGame = function() {
-  if (this.SessionId) {
-    clearInterval(this.SessionId);
-  } // pause game
-  this.isRunning = false;
-};
-
-PinballGame.prototype.startContinueGame = function() {
-  this.SessionId = setInterval(this.gameLoop.bind(this), 1000/this.fps); // start game
-};
-
-
-PinballGame.prototype.drawGame = function() {
-  //Set background to black
-  const backgroundcolor = "rgba(0,0,0,1)";
-  const cxt = this.cxt;
-  cxt.fillStyle = backgroundcolor;
-  cxt.fillRect (0, 0, this.canvas.width, this.canvas.height);
-
-  //DRAW SHIP
-  let fill = true;
-  let radius = 0;
-  cxt.fillStyle = this.ship.color;
-  let rectX = this.ship.pos.x - this.ship.size.w;
-  let rectY = this.ship.pos.y - this.ship.size.h;
-  roundRect(cxt, rectX, rectY, this.ship.size.w, this.ship.size.h, radius, fill);
-  //DRAW BALLS
-  for (var i = 0; i < this.balls.length; i++) {
-    //console.log("this.balls[i].pos", this.balls[i].pos);
-    this.drawCircle(this.balls[i].pos.x, this.balls[i].pos.y, this.balls[i].color, this.balls[i].size);
-  }
-};
-
-PinballGame.prototype.drawCircle = function(posy, posY, color, size, offset) {
-  if (typeof offset === 'undefined' || !offset) var offset = 0;
-  this.cxt.fillStyle = color;
-  this.cxt.beginPath();
-  this.cxt.arc(posy, posY, size, 0, Math.PI*2);
-  this.cxt.closePath();
-  this.cxt.fill();
-};
-
-//
-// PinballGame.prototype.endGame = function() {
-//   if (this.SessionId) {
-//     clearInterval(this.SessionId);
-//   } // pause game
-// };
